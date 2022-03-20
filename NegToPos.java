500345e73f5ffed250efc3f3a2f2a562842c647c
RadhiAug <101966786+RadhiAug@users.noreply.github.com>
2022-03-20 13:48:29 +0530

    Add files via upload

---
+++ NegToPos.java
@@ -0,0 +1,14 @@
+package week1.day1;
+
+public class NegToPos {
+
+	public static void main(String[] args) {
+		
+		int g = -40;
+		int t;
+if (g<=0) {
+	t = g* (-1);
+	System.out.println("The Number "+ g + " is converted to " + t);
+}
+}
+}
